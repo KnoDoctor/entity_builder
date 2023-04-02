5d59a9f74b8bdb1c948f823a11b48416d3278fcc
Jeff Barfield <barfieldjt@gmail.com>
2023-04-01 21:18:37 -0400

    first commit

---
+++ entity_builder.js
@@ -0,0 +1,276 @@
+const fs = require("fs");
+const readline = require("readline");
+
+//API
+const { buildApiIndexFile } = require("./structure/pages/api/[entity]/index");
+const {
+    buildApiIdentifierFile,
+} = require("./structure/pages/api/[entity]/[identifier]");
+
+//Frontend
+const { buildEntityIndexFile } = require("./structure/pages/[entity]/index");
+const {
+    buildEntityIdentifierFile,
+} = require("./structure/pages/[entity]/[...identifier]");
+
+//Hooks
+const { buildUseEntityFile } = require("./structure/hooks/[entity]/useEntity");
+const {
+    buildUseEntitiesFile,
+} = require("./structure/hooks/[entity]/useEntities");
+
+//Components => Organisms
+const {
+    buildEntityOrganismFile,
+} = require("./structure/components/organisms/[entity]/EntityOrganism");
+const {
+    buildEntityListOrganismFile,
+} = require("./structure/components/organisms/[entity]/EntityListOrganism");
+const {
+    buildEntityDeletionOrganismFile,
+} = require("./structure/components/organisms/[entity]/EntityDeletionOrganism");
+const {
+    buildEntityCreationOrganismFile,
+} = require("./structure/components/organisms/[entity]/EntityCreationOrganism");
+
+//Components => _Molecules
+const {
+    buildEntityListCardFile,
+} = require("./structure/components/_molecules/[entity]/EntityListCard");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const baseDir = "./output"; // Base directory to write files
+
+rl.question("Enter an Uppercase Plural: ", (uppercasePlural) => {
+    rl.question("Enter an Uppercase Singular: ", (uppercaseSingular) => {
+        rl.question("Enter a Lowercase Plural: ", (lowercasePlural) => {
+            rl.question("Enter a Lowercase Singular: ", (lowercaseSingular) => {
+                //API
+                const apiDir = `${baseDir}/pages/api/${lowercasePlural}/`;
+
+                const apiIndexFile = `${apiDir}index.ts`;
+                const apiIdentifierFile = `${apiDir}[identifier].ts`;
+
+                const apiIndexContent = buildApiIndexFile({
+                    uppercasePlural,
+                    uppercaseSingular,
+                    lowercasePlural,
+                    lowercaseSingular,
+                });
+                const apiIdentifierContent = buildApiIdentifierFile({
+                    uppercasePlural,
+                    uppercaseSingular,
+                    lowercasePlural,
+                    lowercaseSingular,
+                });
+
+                //Frontend
+                const frontendDir = `${baseDir}/pages/${lowercasePlural}/`;
+
+                const entityIndexFile = `${frontendDir}index.tsx`;
+                const entityIdentifierFile = `${frontendDir}[...identifier].tsx`;
+
+                const entityIndexContent = buildEntityIndexFile({
+                    uppercasePlural,
+                    uppercaseSingular,
+                    lowercasePlural,
+                    lowercaseSingular,
+                });
+                const entityIdentifierContent = buildEntityIdentifierFile({
+                    uppercasePlural,
+                    uppercaseSingular,
+                    lowercasePlural,
+                    lowercaseSingular,
+                });
+
+                //Hooks
+                const hooksDir = `${baseDir}/hooks/${lowercasePlural}/`;
+
+                const useEntityFile = `${hooksDir}use${uppercaseSingular}.ts`;
+                const useEntitiesFile = `${hooksDir}use${uppercasePlural}.ts`;
+
+                const useEntityContent = buildUseEntityFile({
+                    uppercasePlural,
+                    uppercaseSingular,
+                    lowercasePlural,
+                    lowercaseSingular,
+                });
+                const useEntitiesContent = buildUseEntitiesFile({
+                    uppercasePlural,
+                    uppercaseSingular,
+                    lowercasePlural,
+                    lowercaseSingular,
+                });
+
+                //Components => Organisms
+                const componentOrganismDir = `${baseDir}/components/organisms/${lowercasePlural}/`;
+
+                const entityCreationOrganismFile = `${componentOrganismDir}${uppercaseSingular}CreationOrganism.tsx`;
+                const entityDeletionOrganismFile = `${componentOrganismDir}${uppercaseSingular}DeletionOrganism.tsx`;
+                const entityListOrganismFile = `${componentOrganismDir}${uppercaseSingular}ListOrganism.tsx`;
+                const entityOrganismFile = `${componentOrganismDir}${uppercaseSingular}Organism.tsx`;
+
+                const entityOrganismContent = buildEntityOrganismFile({
+                    uppercasePlural,
+                    uppercaseSingular,
+                    lowercasePlural,
+                    lowercaseSingular,
+                });
+                const entityListOrganismContent = buildEntityListOrganismFile({
+                    uppercasePlural,
+                    uppercaseSingular,
+                    lowercasePlural,
+                    lowercaseSingular,
+                });
+                const entityDeletionOrganismContent =
+                    buildEntityDeletionOrganismFile({
+                        uppercasePlural,
+                        uppercaseSingular,
+                        lowercasePlural,
+                        lowercaseSingular,
+                    });
+                const entityCreationOrganismContent =
+                    buildEntityCreationOrganismFile({
+                        uppercasePlural,
+                        uppercaseSingular,
+                        lowercasePlural,
+                        lowercaseSingular,
+                    });
+
+                //Components => _Molecules
+                const componentMoleculesDir = `${baseDir}/components/_molecules/${lowercasePlural}/`;
+
+                const entityListCardFile = `${componentMoleculesDir}${uppercaseSingular}ListCard.tsx`;
+
+                const entityListCardContent = buildEntityListCardFile({
+                    uppercasePlural,
+                    uppercaseSingular,
+                    lowercasePlural,
+                    lowercaseSingular,
+                });
+
+                // Create directories if they don't exist
+                fs.mkdirSync(apiDir, { recursive: true });
+                fs.mkdirSync(frontendDir, { recursive: true });
+                fs.mkdirSync(hooksDir, { recursive: true });
+                fs.mkdirSync(componentOrganismDir, { recursive: true });
+                fs.mkdirSync(componentMoleculesDir, { recursive: true });
+
+                //API
+                fs.writeFile(apiIndexFile, apiIndexContent, (err) => {
+                    if (err) throw err;
+                    console.log("Index file created with name:", apiIndexFile);
+                });
+
+                fs.writeFile(apiIdentifierFile, apiIdentifierContent, (err) => {
+                    if (err) throw err;
+                    console.log(
+                        "Identifier file created with name:",
+                        apiIdentifierFile
+                    );
+                });
+
+                //Frontend
+                fs.writeFile(entityIndexFile, entityIndexContent, (err) => {
+                    if (err) throw err;
+                    console.log(
+                        "Identifier file created with name:",
+                        apiIdentifierFile
+                    );
+                });
+                fs.writeFile(
+                    entityIdentifierFile,
+                    entityIdentifierContent,
+                    (err) => {
+                        if (err) throw err;
+                        console.log(
+                            "Identifier file created with name:",
+                            apiIdentifierFile
+                        );
+                    }
+                );
+
+                //Hooks
+                fs.writeFile(useEntityFile, useEntityContent, (err) => {
+                    if (err) throw err;
+                    console.log(
+                        "Identifier file created with name:",
+                        useEntityFile
+                    );
+                });
+                fs.writeFile(useEntitiesFile, useEntitiesContent, (err) => {
+                    if (err) throw err;
+                    console.log(
+                        "Identifier file created with name:",
+                        useEntitiesFile
+                    );
+                });
+
+                //Components => Organisms
+                fs.writeFile(
+                    entityOrganismFile,
+                    entityOrganismContent,
+                    (err) => {
+                        if (err) throw err;
+                        console.log(
+                            "Identifier file created with name:",
+                            entityOrganismFile
+                        );
+                    }
+                );
+                fs.writeFile(
+                    entityListOrganismFile,
+                    entityListOrganismContent,
+                    (err) => {
+                        if (err) throw err;
+                        console.log(
+                            "Identifier file created with name:",
+                            entityListOrganismFile
+                        );
+                    }
+                );
+                fs.writeFile(
+                    entityDeletionOrganismFile,
+                    entityDeletionOrganismContent,
+                    (err) => {
+                        if (err) throw err;
+                        console.log(
+                            "Identifier file created with name:",
+                            entityDeletionOrganismFile
+                        );
+                    }
+                );
+                fs.writeFile(
+                    entityCreationOrganismFile,
+                    entityCreationOrganismContent,
+                    (err) => {
+                        if (err) throw err;
+                        console.log(
+                            "Identifier file created with name:",
+                            entityCreationOrganismFile
+                        );
+                    }
+                );
+
+                //Components => _Molecules
+                fs.writeFile(
+                    entityListCardFile,
+                    entityListCardContent,
+                    (err) => {
+                        if (err) throw err;
+                        console.log(
+                            "Identifier file created with name:",
+                            entityListCardFile
+                        );
+                    }
+                );
+
+                rl.close();
+            });
+        });
+    });
+});
